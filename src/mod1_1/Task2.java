f937c897dbd87bd9c654a1e3f50fa907efded1fb
k45p4r <kaspar.vainonen@metropolia.fi>
2025-08-18 10:50:06 +0300

    push

--- src/mod1_1/Task2.java
+++ src/mod1_1/Task2.java
@@ -2,11 +2,6 @@ package mod1_1;
 
 public class Task2 {
     public static void main(String [] args) {
-        System.out.println("""
-                    * \n
-                   *** \n
-                  ***** \n
-                 ******* \n
-                """);
+        System.out.println("    *\n" + "   ***\n" + "  *****\n"  + " *******\n");
     }
 }
