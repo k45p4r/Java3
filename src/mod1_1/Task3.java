eb7c7ddd7a0708d8c15945fcf8f57a587fef5bef
k45p4r <kaspar.vainonen@metropolia.fi>
2025-08-18 10:25:41 +0300

    push

--- src/mod1_1/Task3.java
+++ src/mod1_1/Task3.java
@@ -16,6 +16,8 @@ public class Task3 {
         System.out.println("Give the third number:");
         int third = Integer.parseInt(scanner.nextLine());
 
-        System.out.println("The sum of the numbers is " + (first + second + third) + ", the product is " + (first * second * third) + ", and the average is " + (first + second + third) / 3.0);
+        System.out.println("The sum of the numbers is " + (first + second + third) +
+                ", the product is " + (first * second * third) +
+                ", and the average is " + (first + second + third) / 3.0);
     }
 }
